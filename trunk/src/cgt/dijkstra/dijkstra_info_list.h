db0d3fe11f836af764c0788cadb37203ddc8a24f
leandro.costa <leandro.costa@08941fba-b46d-11de-9ad3-fd5786690477>
2009-10-30 01:10:35 +0000

    More for iterators

--- trunk/src/cgt/dijkstra/dijkstra_info_list.h
+++ trunk/src/cgt/dijkstra/dijkstra_info_list.h
@@ -20,77 +20,61 @@ namespace cgt
         typedef _DijkstraInfo<_TpVertex, _TpEdge> _Info;
         typedef _GraphNode<_TpVertex, _TpEdge>    _Node;
         typedef _GraphEdge<_TpVertex, _TpEdge>    _Edge;
-        typedef typename _Base::iterator          _InfoIterator;
+        typedef typename _Base::iterator          _Iterator;
 
       private:
         using _Base::begin;
         using _Base::end;
         using _Base::empty;
 
+      private:
+        static const bool _has_node (const _Info& _info, const _Node& _node) { return (_info.node ().vertex () == _node.vertex ()); }
+
       public:
-        _Info* get_by_node (const _Node* const _ptr_node);
-        _Info* get_closest ();
+        _Iterator get_by_node (const _Node* const _ptr_node);
+        _Iterator get_closest ();
         void relax (const _Node* const _ptr_node, const _TpEdge& _distance, const _Edge& _edge, _Node* const _ptr_prev);
     };
 
   template<typename _TpVertex, typename _TpEdge>
-    _DijkstraInfo<_TpVertex, _TpEdge>* _DijkstraInfoList<_TpVertex, _TpEdge>::get_by_node (const _Node* const _ptr_node)
+    typename _DijkstraInfoList<_TpVertex, _TpEdge>::_Iterator _DijkstraInfoList<_TpVertex, _TpEdge>::get_by_node (const _Node* const _ptr_node)
     {
-      _Info* _ptr = NULL;
-
-      _InfoIterator it;
-      _InfoIterator itEnd = end ();
-
-      for (it = begin (); it != itEnd; ++it)
-      {
-        if (it->node ().vertex () == _ptr_node->vertex ())
-        {
-          _ptr = &(*it);
-          break;
-        }
-      }
-
-      return _ptr;
+      return find_if (begin (), end (), _has_node, *_ptr_node);
     }
 
   template<typename _TpVertex, typename _TpEdge>
-    _DijkstraInfo<_TpVertex, _TpEdge>* _DijkstraInfoList<_TpVertex, _TpEdge>::get_closest ()
+    typename _DijkstraInfoList<_TpVertex, _TpEdge>::_Iterator _DijkstraInfoList<_TpVertex, _TpEdge>::get_closest ()
     {
-      _Info* _ptr = NULL;
-
-      _InfoIterator it    = begin ();
-      _InfoIterator itEnd = end ();
+      _Iterator it_ret = begin ();
 
       if (! empty ())
       {
-        _ptr = &(*it);
+        _Iterator it = it_ret;
+        _Iterator itEnd = end ();
 
         for (++it; it != itEnd; ++it)
         {
-          if (! it->inf_distance ())
-          {
-            if (*it < *_ptr)
-              _ptr = &(*it);
-          }
+          if (! it->inf_distance () && *it < *it_ret)
+            it_ret = it;
         }
       }
 
-      return _ptr;
+      return it_ret;
     }
 
   template<typename _TpVertex, typename _TpEdge>
     void _DijkstraInfoList<_TpVertex, _TpEdge>::relax (const _Node* const _ptr_node, const _TpEdge& _distance, const _Edge& _edge, _Node* const _ptr_prev)
     {
-      _Info *_ptr_i = get_by_node (_ptr_node);
+      _Iterator it = get_by_node (_ptr_node);
 
-      if (_ptr_i)
+      if (it != end ())
       {
         _TpEdge new_distance = _distance + _edge.value ();
 
-        if (_ptr_i->inf_distance () || _ptr_i->distance () > new_distance)
+        if (it->inf_distance () || it->distance () > new_distance)
         {
-          _ptr_i->_set_distance (new_distance);
-          _ptr_i->_set_previous (_ptr_prev);
+          it->_set_distance (new_distance);
+          it->_set_previous (_ptr_prev);
         }
       }
     }
