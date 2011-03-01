9fc5057d4c9a72d00731f82b7608c65de2d37558
leandro.costa@gmail.com <leandro.costa@gmail.com@08941fba-b46d-11de-9ad3-fd5786690477>
2011-03-01 11:28:45 +0000

    Still working on bellman-ford algorithm

---
+++ src/cgt/shortpath/single/bellford/bellford_info_list.h
@@ -0,0 +1,159 @@
+/*
+ * CGTL - A graph template library for C++
+ * ---------------------------------------
+ * Copyright (C) 2009 Leandro Costa
+ *
+ * This file is part of CGTL.
+ *
+ * CGTL is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * CGTL is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with CGTL. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*!
+ * \file cgt/shortpath/single/bellford/bellford_info_list.h
+ * \brief Containss definition of a list of structures used by Bellford iterator.
+ * \author Leandro Costa
+ * \date 2009
+ *
+ * $LastChangedDate: 2011-02-16 14:36:44 -0200 (Qua, 16 Fev 2011) $
+ * $LastChangedBy: leandro.costa@gmail.com $
+ * $Revision: 124 $
+ */
+
+#ifndef __CGTL__CGT_SHORTPATH_SINGLE_DIJKSTRA_DIJKSTRA_INFO_LIST_H_
+#define __CGTL__CGT_SHORTPATH_SINGLE_DIJKSTRA_DIJKSTRA_INFO_LIST_H_
+
+#include "cgt/graph_node.h"
+#include "cgt/shortpath/single/bellford/bellford_info.h"
+
+#ifdef CGTL_DO_NOT_USE_STL
+#include "cgt/base/list.h"
+#else
+#include <list>
+
+template<typename _TpIter, typename _Predicate, typename _Parm>
+_TpIter find_if (_TpIter _it, _TpIter _end, _Predicate _pred, const _Parm& _parm)
+{
+	while (_it != _end && ! _pred (*_it, _parm))
+		++_it;
+
+	return _it;
+}
+
+#endif
+
+
+namespace cgt
+{
+  namespace shortpath
+  {
+    namespace single
+    {
+      namespace bellford
+      {
+        /*!
+         * \class _BellfordInfoList
+         * \brief A list of bellford info structures used by bellford iterator.
+         * \author Leandro Costa
+         * \date 2009
+         *
+         * The list used by Bellford Algorithm to keep the nodes already taken.
+         * This is also the list of bellford infos that can be obtained after
+         * the algorithm finish.
+         */
+
+        template<typename _TpVertex, typename _TpEdge>
+#ifdef CGTL_DO_NOT_USE_STL
+          class _BellfordInfoList : public cgt::base::list<_BellfordInfo<_TpVertex, _TpEdge> >
+#else
+          class _BellfordInfoList : public std::list<_BellfordInfo<_TpVertex, _TpEdge> >
+#endif
+        {
+          private:
+            typedef _BellfordInfo<_TpVertex, _TpEdge>     _Info;
+            typedef _BellfordInfoList<_TpVertex, _TpEdge> _Self;
+#ifdef CGTL_DO_NOT_USE_STL
+            typedef cgt::base::list<_Info>                           _Base;
+#else
+            typedef std::list<_Info>                           _Base;
+#endif
+
+          private:
+            typedef _GraphNode<_TpVertex, _TpEdge>    _Node;
+            typedef _GraphEdge<_TpVertex, _TpEdge>    _Edge;
+            typedef typename _Base::iterator          _Iterator;
+
+          private:
+            static const bool _has_node (const _Info& _info, const _Node& _node) { return (_info.node ().vertex () == _node.vertex ()); }
+
+          public:
+            //_Iterator get_by_node (const _Node* const _ptr_node);
+            //_Iterator get_closest ();
+            //void relax (const _Node* const _ptr_node, const _TpEdge& _distance, const _Edge& _edge, _Node* const _ptr_prev);
+        };
+
+		/*
+        template<typename _TpVertex, typename _TpEdge>
+          typename _BellfordInfoList<_TpVertex, _TpEdge>::_Iterator _BellfordInfoList<_TpVertex, _TpEdge>::get_by_node (const _Node* const _ptr_node)
+          {
+            return find_if (_Base::begin (), _Base::end (), _has_node, *_ptr_node);
+          }
+		  */
+
+		  /*
+        template<typename _TpVertex, typename _TpEdge>
+          typename _BellfordInfoList<_TpVertex, _TpEdge>::_Iterator _BellfordInfoList<_TpVertex, _TpEdge>::get_closest ()
+          {
+            _Iterator it_ret = _Base::begin ();
+
+            if (! _Base::empty ())
+            {
+              _Iterator it = it_ret;
+              _Iterator itEnd = _Base::end ();
+
+              for (++it; it != itEnd; ++it)
+              {
+                if (! it->inf_distance () && *it < *it_ret)
+                  it_ret = it;
+              }
+            }
+
+            return it_ret;
+          }
+		  */
+
+		 /*
+        template<typename _TpVertex, typename _TpEdge>
+          void _BellfordInfoList<_TpVertex, _TpEdge>::relax (const _Node* const _ptr_node, const _TpEdge& _distance, const _Edge& _edge, _Node* const _ptr_prev)
+          {
+            _Iterator it = get_by_node (_ptr_node);
+
+            if (it != _Base::end ())
+            {
+              _TpEdge new_distance = _distance + _edge.value ();
+
+              if (it->inf_distance () || it->distance () > new_distance)
+              {
+                it->_set_distance (new_distance);
+                it->_set_previous (_ptr_prev);
+              }
+            }
+          }
+		  */
+      }
+    }
+  }
+}
+
+
+#endif // __CGTL__CGT_SHORTPATH_SINGLE_DIJKSTRA_DIJKSTRA_INFO_LIST_H_
